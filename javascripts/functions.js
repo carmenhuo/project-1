cbf94e5b2c8e739db83812327296e55d3343d305
Carmen Ho <carmenh@uoregon.edu>
2020-04-10 22:52:40 -0700

    Add sumEvens to functions.js. 

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,5 +1,5 @@
 // jshint esversion: 6
-// Req. 3b
+// Req. 3b returns averages
 let arrAvg = arr => {
   let sum = 0;
   for (var i = 0; i < arr.length; i++) {
@@ -7,7 +7,7 @@ let arrAvg = arr => {
   }
   return sum / arr.length;
 };
-//Req. 3c array max
+//Req. 3c array max returns largest number
 
 let arrMax = arr => {
   let max = 0;
@@ -15,4 +15,15 @@ let arrMax = arr => {
     if (max < elem) max = elem;
   });
   return max;
+};
+
+//Req. 3d sumEvens sum of all even numbers
+let sumEvens = num => {
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 === 0) {
+      sum = sum + num[i];
+    }
+  }
+  return sum;
 };
