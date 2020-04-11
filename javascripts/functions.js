37d368c854aeb73fe5f3b6f574519b6d154bc2c7
Carmen Ho <carmenh@uoregon.edu>
2020-04-10 22:15:29 -0700

    Add arrAvg to functions.js.

---
+++ javascripts/functions.js
@@ -0,0 +1,15 @@
+// jshint esversion: 6
+// Req. 3b
+let arrAvg = arr => {
+  let sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum / arr.length;
+};
+//Req. 3c
+
+let arrMax = arr => {
+  //
+};
+//===================
